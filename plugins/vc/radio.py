bc6c95e8a309e39c2f57893fd77e052386fbba69
callmeA1 <82321816+callmeA1@users.noreply.github.com>
2021-08-20 21:11:48 +0530

    Update radio.py

--- plugins/vc/radio.py
+++ plugins/vc/radio.py
@@ -104,5 +104,5 @@ async def leaveradio(_, message: Message):
 
 @Client.on_message(self_or_contact_filter & filters.command('radio', prefixes='!'))
 async def show_radio_help(_, m: Message):
-    await m.reply_text(f"- [Some Live Stream Links Here](https://telegra.ph/Some-Radio-Links-08-17) \n- You can also use YT-LIVE URL \n\n`!stream [stream_url]`  -  __Starts Live Stream from that Link__ \n`!end`  -  __Stops the Live Stream__", disable_web_page_preview=True)
+    await m.reply_text(f"- [Some Live Stream Links Here](https://telegra.ph/file/4bd99698b050f06062b3e.jpg) \n- You can also use YT-LIVE URL \n\n`!stream [stream_url]`  -  __Starts Live Stream from that Link__ \n`!end`  -  __Stops the Live Stream__", disable_web_page_preview=True)
 
